efec033df34e7061910fd5cefc6b7cd64e706839
Zoltan Dalmadi <dmz985@gmail.com>
2017-10-14 11:43:28 +0200

    Added more operations

--- src/one.js
+++ src/one.js
@@ -1,2 +1,5 @@
 export const one = (value) => value * value;
 export const heavy = (value, otherValue) => value * otherValue;
+export const heavy2 = (value, otherValue) => value + otherValue;
+export const heavy3 = (value, otherValue) => value - otherValue;
+export const heavy4 = (value, otherValue) => value / otherValue;
